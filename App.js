3a9ea6eac970a46c47117a6f9ebe2d152a759fdc
adrian hernandez <adrianhh1426@gmail.com>
2019-12-22 21:40:57 -0600

    Picker

--- App.js
+++ App.js
@@ -1,57 +1,38 @@
 import React from 'react';
-import {Text, View, FlatList, StyleSheet} from 'react-native';
+import {Text, View, Picker, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container:{
     display: 'flex',
-    flex: 1,
-    justifyContent:'center',
-    alignItems:'center'
+    flex:1,
+    justifyContent:'center'
+  },
+  picker:{
+    height:70,
+    width:150
   }
 });
 
 export default class App extends React.Component {
-
-  state={
-    loading: true,
-    users:[]
-  }
-
-  constructor(props){
-    super(props);
-    this.fetchUsers();
-  }
-
-  fetchUsers = async () =>{
-   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-   const preUsers = await response.json();
-   const users = preUsers.map(x => ({...x, key:String(x.id)}));
-   this.setState({
-     users,
-     loading: false
-   });
-  }
-
+  state={};
 render(){
-  const {loading, users} = this.state;
-
-  if(loading){
-    return(
-      <View style={styles.container}>
-        <Text>Cargando....</Text>
-      </View>
-    );
-  }
-
   return(
-  <View>
-    <FlatList
-      data={users}
-      renderItem={({item})=>
-        <Text>{item.name}</Text>
-      }
-    />
-  </View>
+    <View style={styles.container}>
+      <Picker 
+        selectedValue={this.state.value || "id_1"}
+        style={styles.picker}
+        onValueChange={(value,index)=>{
+          this.setState({
+            value
+          });
+        }}
+      >
+        <Picker.Item label="Opcion 1"value="id_1"/>
+        <Picker.Item label="Opcion 2"value="id_2"/>
+        <Picker.Item label="Opcion 3"value="id_3"/>
+        <Picker.Item label="Opcion 4"value="id_4"/>
+      </Picker>
+    </View>
   );
 }
 }
