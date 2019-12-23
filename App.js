6a9f4ba0583a1f974a1feb1c390a5a5ced2dc680
adrian hernandez <adrianhh1426@gmail.com>
2019-12-22 19:17:39 -0600

    SectionList

--- App.js
+++ App.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import {Text, View, FlatList} from 'react-native';
+import {Text, View, SectionList} from 'react-native';
 
 export default class App extends React.Component {
 render(){
   return (
     <View>
-      <FlatList
-        data={[
-          {name:'Memphis' , key:'1'},
-          {name:'Hazard' , key:'2'},
-          {name:'Vinicius' , key:'3'},
-          {name:'Mbappe' , key:'4'},
-          {name:'Cavani' , key:'5'},
-          {name:'Giniac' , key:'6'},
-          {name:'Valencia' , key:'7'},
-          {name:'Dos santos' , key:'8'},
-          {name:'Cavani' , key:'9'},
-          {name:'Giniac' , key:'10'},
-          {name:'Valencia' , key:'11'},
-          {name:'Dos santos' , key:'12'}
+      <SectionList
+        sections={[
+          {
+            title:"Nombres",
+            data:[
+              {name:'Memphis' , key:'1'},
+              {name:'Hazard' , key:'2'},
+              {name:'Vinicius' , key:'3'},
+              {name:'Mbappe' , key:'4'},
+              {name:'Cavani' , key:'5'}
+            ]
+          },
+          {
+            title:"Giniacs",
+            data:[
+              {name:'Giniac' , key:'6'},
+              {name:'Valencia' , key:'7'},
+              {name:'Dos santos' , key:'8'},
+              {name:'Cavani' , key:'9'},
+              {name:'Giniac' , key:'10'},
+              {name:'Valencia' , key:'11'},
+              {name:'Dos santos' , key:'12'}
+            ]
+          }
         ]}
-        renderItem={
-          ({item})=><Text style={{fontSize: 80}}>{item.name}</Text>
+        renderItem={({item})=> 
+        <Text style={{fontSize:50}}>{item.name}</Text>
+        }
+        renderSectionHeader={({section})=>
+        <Text style={{fontSize: 40, backgroundColor:'gray'}}>{section.title}</Text>
         }
       />
     </View>
