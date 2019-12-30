df2d925c1236978b751396cdfc0902657a84b221
adrian hernandez <adrianhh1426@gmail.com>
2019-12-29 18:24:36 -0600

    Camera

--- App.js
+++ App.js
@@ -1,36 +1,60 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button,TouchableOpacity, StyleSheet} from 'react-native';
 import * as Permissions from 'expo-permissions';
-import * as Location from 'expo-location';
+import {Camera} from 'expo-camera';
 
 export default class App extends React.Component {
 
   state={
-    location: {coords:{}},
-    errorMessage: null
+    perm: null,
+    typeCamera: Camera.Constants.Type.back
   }
 
-  getLocation = async () =>{
-    const {status} = await Permissions.askAsync(Permissions.LOCATION);
-    if (status!== "granted"){
-      return this.setState({errorMessage:'Permisos denegados'});
+  async componentDidMount(){
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({perm: status});
+  }
+
+  cambiarCamara = () =>{
+    const {Type} = Camera.Constants;
+    const typeCamera = Type.back === this.state.typeCamera ? Type.front : Type.back;
+    this.setState({typeCamera});
+  }
+
+  tomarFoto = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      console.log("foto: ",photo)
     }
-
-    const location= await Location.getCurrentPositionAsync();
-    this.setState({location});
-  }
+  };
 
 render(){
-  const {location} = this.state;
-  return(
-    <View style={estilos.container}>
-      <Text>
-        {`latitud: ${location.coords.latitude}`}
-        {` longitud: ${location.coords.longitude}`}
-      </Text>
-      <Button title="solicitar permiso" onPress={this.getLocation}/>
-    </View>
-  );
+  const {perm,typeCamera} = this.state;
+    if(perm===null){
+      return <View style={estilos.container}/>
+    }
+    else if(perm==='denegaded'){
+     return(<View style={estilos.container}><Text>Dame permisos!!!</Text></View>);
+    }
+    else{
+      return(
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={typeCamera} ref={ref =>{this.camera=ref}}> 
+            <View style={{
+              flex:1,
+              backgroundColor: 'transparent',
+              flexDirection:'row',
+              justifyContent:'center',
+              alignItems:'flex-end',
+              padding:10
+            }}>
+              <Button title="cambiar camara" onPress={this.cambiarCamara}/>
+              <Button title="tomar foto" onPress={this.tomarFoto}/>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
 }
 }
 
